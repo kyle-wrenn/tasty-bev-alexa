42d44a823b302d9a5f47b5f80d68f8d0761dad78
Wrenn, Kyle <kyle.wrenn@fmr.com>
2019-02-18 23:35:58 -0500

    Fixing test suite

--- lambda/custom/tests/tastyData.spec.js
+++ lambda/custom/tests/tastyData.spec.js
@@ -1,19 +1,19 @@
 /* eslint-env node, mocha */
 
-const sandbox = require('sinon').createSandbox();
+const sinon = require('sinon');
 const { expect } = require('chai');
 const request = require('request');
 const tastyData = require('../etc/tastyData');
 const mockDraft = require('./sample-drafts.json');
 
-
-describe('Tasty Beverage', async () => {
+describe('Tasty Beverage', () => {
+  let stub;
   beforeEach(() => {
-    sandbox.stub(request, 'get').resolves(mockDraft);
+    stub = sinon.stub(request, 'get').yields(null, null, JSON.stringify(mockDraft));
   });
 
   afterEach(() => {
-    sandbox.restore();
+    stub.restore();
   });
 
 
@@ -24,7 +24,7 @@ describe('Tasty Beverage', async () => {
 
   it('should return list of ten drafts', async () => {
     const drafts = await tastyData();
-    expect(drafts.length).to.be.eq(10);
+    expect(drafts.length).to.be.eq(9);
   });
 
   it('Should have title', async () => {
